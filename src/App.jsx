5483f1559a27036bb6fa23b214b1f7d82786076f
jayashre-25 <dasjayashree8260@gmail.com>
2025-03-06 22:21:05 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import Navbar from "./components/Navbar"
+import Hero from "./components/Hero"
+import Homecards from "./components/Homecards"
+
+const App = () => {
+  return (
+    <>
+       <Navbar />
+       <Hero title='Become a React Dev' subtitle='Find jobs that fits your skill set' />
+       <Homecards />
+    </>
+  )
+}
+
+export default App
