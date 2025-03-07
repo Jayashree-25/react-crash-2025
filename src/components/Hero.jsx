1bd6981fdf9362ed4b7bb203fe27b120d189c18b
jayashre-25 <dasjayashree8260@gmail.com>
2025-03-07 22:52:57 +0530

    changes

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,4 +1,5 @@
 const Hero = ({ title, subtitle }) => {
+    
     return (
         <section className="bg-indigo-700 py-10 mb-4">
             <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 flex flex-col items-center">
