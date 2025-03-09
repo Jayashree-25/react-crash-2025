59b1295e7358c96ed4ccfb5bed728480c6362e16
jayashre-25 <dasjayashree8260@gmail.com>
2025-03-09 10:25:22 +0530

    toggle less/more added

--- src/components/JobListing.jsx
+++ src/components/JobListing.jsx
@@ -1,6 +1,13 @@
-import React from 'react'
+import { useState } from 'react'
 
 const JobListing = ({ job }) => {
+    const [showFullDescription, setShowFullDescription] = useState(false);
+    let description = job.description;
+
+    if (!showFullDescription) {
+        description = description.substring(0, 90) + '...';
+    }
+
     return (
         <div className='bg-white rounded-xl shadow-md relative'>
             <div className='p-4'>
@@ -13,9 +20,11 @@ const JobListing = ({ job }) => {
                     </h3>
                 </div>
 
-                <div>
-                    {job.description}
+                <div className='mb-5'>
+                    {description}
                 </div>
+
+                <button onClick={() => setShowFullDescription((prevState) => !prevState)} className='text-indigo-500 mb-5 hover:text-indigo-600'>{showFullDescription ? 'Less' : 'More'}</button>
 
                 <h3 className='text-indigo-500 mb-2'>{job.salary} / Year</h3>
 
@@ -23,7 +32,7 @@ const JobListing = ({ job }) => {
 
                 <div className='flex flex-col lg:flex-row justify-between mb-4'>
                     <div className='text-orange-700 mb-3'>
-                        <i className='fa-solid fa-location-dot text-lg'>{job.location}</i>
+                        <i className='fa-solid fa-location-dot text-lg'></i>{job.location}
                     </div>
                     <a href={`/job/${job.id}`} className='h-[36px] bg-indigo-500 hover:bg-indigo-600 text-white px-4 py-2 rounded-l text-center text-sm'>Read More</a>
                 </div>
