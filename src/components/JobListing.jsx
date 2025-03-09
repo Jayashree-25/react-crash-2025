8d6292903add5b764103622eabe6ea67b9c6014c
jayashre-25 <dasjayashree8260@gmail.com>
2025-03-09 20:24:38 +0530

    JobListing buttons

--- src/components/JobListing.jsx
+++ src/components/JobListing.jsx
@@ -1,5 +1,6 @@
 import { useState } from 'react'
 import { FaMapMarker } from 'react-icons/fa'
+import {Link} from 'react-router-dom'
 
 const JobListing = ({ job }) => {
     const [showFullDescription, setShowFullDescription] = useState(false);
@@ -36,7 +37,7 @@ const JobListing = ({ job }) => {
                         <FaMapMarker className='inline text-lg mb-1 mr-1' />
                         {job.location}
                     </div>
-                    <a href={`/job/${job.id}`} className='h-[36px] bg-indigo-500 hover:bg-indigo-600 text-white px-4 py-2 rounded-l text-center text-sm'>Read More</a>
+                    <Link to={`/job/${job.id}`} className='h-[36px] bg-indigo-500 hover:bg-indigo-600 text-white px-4 py-2 rounded-l text-center text-sm'>Read More</Link>
                 </div>
             </div>
         </div>
