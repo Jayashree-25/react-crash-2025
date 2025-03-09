3e11702f41b6f5d9bc220e819896fb1944e34d9d
jayashre-25 <dasjayashree8260@gmail.com>
2025-03-09 19:42:50 +0530

    outlet

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,33 @@
-import { 
-  Route, 
-  createBrowserRouter, 
-  createRoutesFromElements, 
-  RouterProvider 
+import {
+  Route,
+  createBrowserRouter,
+  createRoutesFromElements,
+  RouterProvider
 } from 'react-router-dom';
 import HomePage from './pages/HomePage';
+import MainLayout from './layouts/MainLayout';
 
 const router = createBrowserRouter(
   createRoutesFromElements(
-    <Route path="/" element={<HomePage />} />
+    <Route path="/" element={<MainLayout />}>
+      <Route index element={<HomePage />} />
+    </Route>
   )
 );
 
 const App = () => {
   return (
     <RouterProvider router={router} />
-  // (
-  //   <>
-  //     <Navbar />
-  //     <Hero title='Become a React Dev' subtitle='Find jobs that fits your skill set' />
-  //     <Homecards />
-  //     <JobListings />
-  //     <ViewAllJobs />
-  //   </>
-  // )
-)}
+    // (
+    //   <>
+    //     <Navbar />
+        // <Hero title='Become a React Dev' subtitle='Find jobs that fits your skill set' />
+    //     <Homecards />
+    //     <JobListings />
+    //     <ViewAllJobs />
+    //   </>
+    // )
+  )
+}
 
 export default App
--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -4,8 +4,15 @@ const Hero = ({ title, subtitle }) => {
         <section className="bg-indigo-700 py-10 mb-4">
             <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 flex flex-col items-center">
                 <div className="text-center">
+                    <h1 className="text-4xl font-extrabold text-white sm:text-5xl md:text-6xl">Become a React Dev</h1>
+                    <p className="my-4 text-xl text-white">Find jobs that fits your skill set</p>
+
+
+                    {/* else you can do like this..
                     <h1 className="text-4xl font-extrabold text-white sm:text-5xl md:text-6xl">{title}</h1>
-                    <p className="my-4 text-xl text-white">{subtitle}</p>
+                    <p className="my-4 text-xl text-white">{subtitle}</p> */}
+
+                    
                 </div>
             </div>
         </section>
