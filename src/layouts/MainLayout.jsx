c94baa40ef69b78f7d4fda6657ebb048990b6043
jayashre-25 <dasjayashree8260@gmail.com>
2025-03-09 19:48:58 +0530

    cleared

--- src/layouts/MainLayout.jsx
+++ src/layouts/MainLayout.jsx
@@ -1,12 +1,13 @@
-import React from 'react'
-import {Outlet} from 'react-router-dom'
+import Navbar from '../components/Navbar'
+import { Outlet } from 'react-router-dom'
 
 const MainLayout = () => {
-  return (
-    <>
-        <Outlet />
-    </>
-  )
+    return (
+        <>
+            <Navbar />
+            <Outlet />
+        </>
+    )
 }
 
 export default MainLayout
