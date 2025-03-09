926fb338b98d4f6557ba1c7f5f32099b2e0d9925
jayashre-25 <dasjayashree8260@gmail.com>
2025-03-09 17:17:13 +0530

    react router

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,29 @@
+import {
+  Route, createBrowserRouter, createRoutesFromElements, RouterProvider
+}
+  from 'react-router-dom'
 import Navbar from "./components/Navbar"
 import Hero from "./components/Hero"
 import Homecards from "./components/Homecards"
 import JobListings from "./components/jobListings"
 import ViewAllJobs from "./components/ViewAllJobs"
 
-const App = () => {
-  return (
-    <>
-      <Navbar />
-      <Hero title='Become a React Dev' subtitle='Find jobs that fits your skill set' />
-      <Homecards />
-      <JobListings />
-      <ViewAllJobs />
-    </>
+const router = createBrowserRouter(
+  createRoutesFromElements(<Route index element={ <h1>My app</h1> } />)
+)
 
-  )
+
+const App = () => {
+  return <RouterProvider router={router}/>
+  // (
+  //   <>
+  //     <Navbar />
+  //     <Hero title='Become a React Dev' subtitle='Find jobs that fits your skill set' />
+  //     <Homecards />
+  //     <JobListings />
+  //     <ViewAllJobs />
+  //   </>
+  // )
 }
 
 export default App
