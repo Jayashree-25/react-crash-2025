707ea32c128ad18cf8431f6d60af44789c88e204
jayashre-25 <dasjayashree8260@gmail.com>
2025-03-10 21:37:43 +0530

    small change

--- src/components/ViewAllJobs.jsx
+++ src/components/ViewAllJobs.jsx
@@ -6,7 +6,6 @@ const ViewAllJobs = () => {
     <section className="m-auto max-w-lg my-0 px-6">
         <Link to="/jobs" className="block bg-black text-white text-center py-4 px-6 rounded-xl hover:bg-gray-700">View All Jobs</Link>
       </section>
-
   )
 }
 
